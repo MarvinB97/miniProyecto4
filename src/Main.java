61801dc558ec1ada4be3e1b43622f19ea904edc9
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2023-12-03 19:02:47 -0500

    03-12-23 Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -44,17 +44,44 @@ public class Main {
         //View vista = new View();
         //Controller controller = new Controller(model, vista);
     	
-    	proveedores.add(new Proveedores("12332","Jose","Correa","3123442246","Cra. 15 10-54"));
-    	
-    	String txt = proveedores.get(0).readInformation();
+    	proveedores.add(new Proveedores("12332234","Jose","Correa","3123442246","Cra. 15 10-54"));
+    	proveedores.add(new Proveedores("17363678","Pedro","Marin","3121419849","Calle 6B 11-35"));
+    	proveedores.add(new Proveedores("14634439","Daniel","Perez","3637392321","Cra. 72C 47-26"));
+
+    	//String txt = proveedores.get(0).readInformation();
     	
     	//System.out.println(txt);
+    	ArrayList<String> prov = new ArrayList<String>();
     	
+    	for(int i=0;i<proveedores.size();i++) {
+    		prov.add(proveedores.get(i).lineInformation());
+    	}
     	
-    	file.readFile("Proveedores");
+    	file.writeFile("Proveedores", prov.get(0));
+    	file.writeFile("Proveedores", prov.get(1));    	
+    	
+    	System.out.println("Datos escritos");
+    	System.out.println(file.readFile("Proveedores"));
+    	
+    	file.updateFile("Proveedores", prov);
+    	
+    	System.out.println("Datos actualizados");    	
+    	System.out.println(file.readFile("Proveedores"));
     	
     	
     	
     }
     
 }
+
+//Informacion importante
+/*Los txt tienen las columnas con los siguientes nombres
+ * Proveedores.txt
+ * Id	Nombre	Apellido	Tel	Direccion
+ * Productos.txt
+ * codProductos IdProveedor	costoProductos	cantProductos
+ */
+
+
+
+
